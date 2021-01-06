982340e3a5de049f2a57a362d5fb3802d45c7143
Yun Feng <yun.feng@smartx.com>
2021-01-06 16:27:49 +0800

    build: use electron-rebuild to generate pty.node

--- scripts/install.js
+++ scripts/install.js
@@ -1,18 +1,15 @@
-'use strict'
+'use strict';
 
-const os = require('os');
+const electron_rebuild = require('electron-rebuild');
 const path = require('path');
-const spawn = require('child_process').spawn;
 
-const gypArgs = ['rebuild'];
-if (process.env.NODE_PTY_DEBUG) {
-  gypArgs.push('--debug');
-}
-const gypProcess = spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', gypArgs, {
-  cwd: path.join(__dirname, '..'),
-  stdio: 'inherit'
-});
-
-gypProcess.on('exit', function (code) {
-  process.exit(code);
-});
+electron_rebuild
+  .rebuild({
+    buildPath: path.join(__dirname, '..'),
+    electronVersion: '11.1.1',
+  })
+  .then(() => console.info('Rebuild Successful'))
+  .catch(e => {
+    console.error("Building modules didn't work!");
+    console.error(e);
+  });
