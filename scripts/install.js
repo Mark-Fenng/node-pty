cb56630b8136da112a1706324a07e1e355ee4258
Mark-fenng <f18846188605@gmail.com>
2021-02-22 16:13:52 +0800

    build: set the module path

--- scripts/install.js
+++ scripts/install.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 electron_rebuild
   .rebuild({
-    buildPath: path.join(__dirname, '..'),
+    buildPath: path.join(__dirname, '../'),
+    modulePath: path.join(__dirname, '../build/Release'),
     electronVersion: '11.1.1',
   })
   .then(() => console.info('Rebuild Successful'))
